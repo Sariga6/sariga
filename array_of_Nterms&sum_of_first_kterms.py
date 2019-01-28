f7d89bb96d3dde75d7bd5ca8947163eb3fecd09a
Sariga6 <46817341+Sariga6@users.noreply.github.com>
2019-01-28 21:21:42 +0530

    Create array_of_Nterms&sum_of_first_kterms.py

---
+++ array_of_Nterms&sum_of_first_kterms.py
@@ -0,0 +1,11 @@
+a=[]
+n,add=map(int,input("Enter limit and sum").split(' '))
+for i in range(0,n):
+    s=int(input("enter the number"))
+    a.append(s)
+print(a)
+sum1=0
+for x in range(0,add+1):
+        sum1=sum1+x
+print(sum1)
+    
