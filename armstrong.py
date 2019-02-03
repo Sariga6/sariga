6d5cfdb23e3578fb62590ec9200137b82657e721
Sariga6 <46817341+Sariga6@users.noreply.github.com>
2019-02-03 10:27:27 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -4,14 +4,20 @@ def digits(s):
 n=int(input("enter the number"))
 sum1=0
 temp=n
-while temp!=0:
-    r=temp%10
-    r1=r**digits(n)
-    sum1=sum1+r1
-    temp=temp//10
-if(n==sum1):
-    print("yes")
-else:
-    print("no")
+if n<=100000:
+    while temp!=0:
+        r=temp%10
+        r1=r**digits(n)
+        sum1=sum1+r1
+        temp=temp//10
+    if(n==sum1):
+        print("yes")
+    else:
+        print("no")
+    
+    
+
+    
+
     
     
