00ba36daefeba33d70be3d39379febdf450a52c0
Sariga6 <46817341+Sariga6@users.noreply.github.com>
2019-02-03 08:44:10 +0530

    Create prime_interval.py

---
+++ prime_interval.py
@@ -0,0 +1,8 @@
+q,n=map(int,input("enter the range").split(' '))
+if n<=10000:
+    for j in range(q+1,n):
+        for i in range(2,j):
+            if(j%i==0):
+                break
+        else:
+            print(j,end=' ')
