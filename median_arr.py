478ea4af830b6cd9ba425d75db4b66102273ab65
Sariga6 <46817341+Sariga6@users.noreply.github.com>
2019-02-09 19:26:41 +0530

    Create median_arr.py

---
+++ median_arr.py
@@ -0,0 +1,10 @@
+a=[]
+n=int(input("how many elements to be displayed"))
+if (n<1000 and n>=1):
+    for i in range(0,n):
+        c=int(input("enter the elements"))
+        a.append(c)
+b=sorted(a)
+median=b[len(b)//2]
+print(median)
+
