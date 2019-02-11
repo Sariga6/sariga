a89a3ef2a024c565801db3b6ffea3a6d4c11958b
Sariga6 <46817341+Sariga6@users.noreply.github.com>
2019-02-11 19:42:38 +0530

    Create time_sub.py

---
+++ time_sub.py
@@ -0,0 +1,7 @@
+a,b =map(int,input("enter the time in hr and mins").split(' '))
+c,d =map(int,input("enter the time in hr and mins").split(' '))
+if (a,c)!=0:
+    f=a-c
+    e=b-d
+    print(abs(f),abs(e))
+    
